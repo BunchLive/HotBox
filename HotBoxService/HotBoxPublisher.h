9da06e1cc0af48ddc8e572e7f1505a97c8f590b8
George Lim <george@500labs.com>
2017-08-22 17:10:14 -0400

    Add HotBoxNativeService with React Native bridging

---
+++ HotBoxService/HotBoxPublisher.h
@@ -0,0 +1,12 @@
+//
+//  HotBoxPublisher.h
+//  hotbox
+//
+//  Created by George Lim on 2017-08-08.
+//  Copyright © 2017 George Lim. All rights reserved.
+//
+
+#import <React/RCTView.h>
+
+@interface HotBoxPublisher : RCTView
+@end
