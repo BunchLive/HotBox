6e498a1696e2212082cfc10ab5babd44971c6630
Jordan Howlett <jordan@howlett.io>
2017-08-25 14:47:57 -0400

    Add borderWidth prop for talking border indicator

--- src/PublisherView.js
+++ src/PublisherView.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import  { requireNativeComponent, View } from 'react-native';
 
 class PublisherView extends React.Component {
     static propTypes = {
+        borderWidth: PropTypes.number,
         ...View.propTypes
     }
   render() {
